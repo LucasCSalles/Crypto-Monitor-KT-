148297b4911687b3fd0673008e32ef28afa56598
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-07 21:33:24 -0300

    Firts commit with inital code made in class 07/04/2026

---
+++ app/src/main/java/lucascsalles/com/github/CryptoMonitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,10 @@
+package lucascsalles.com.github.CryptoMonitor.service
+
+import lucascsalles.com.github.CryptoMonitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
